044353f7e9910f2b2556c91bdfb3be5c292ffbfb
tikcuf2 <jared.martin@bryanuniversty.edu>
2021-04-25 14:07:00 -0700

    style list

---
+++ week2/assingment1/app.js
@@ -0,0 +1,20 @@
+var dragonBallz = [
+    "Goku",
+    "Trucks",
+    "Vegta",
+    "Gohan"
+ ]
+ 
+ var DragonballzList = document.getElementById("Dragonballz")
+ 
+ for (var i = 0; i < dragonBallz.length; i++) {
+     dragonBallzList.innerHTML += "<li>" + dragonBallz[i] + "</li>"
+ }
+    
+
+
+
+
+
+
+
